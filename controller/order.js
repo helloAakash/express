7846ae616e40c5abccf87def6e444b6d1a3eef1d
helloAakash <asissunuwar78@gmail.com>
2021-05-04 14:57:28 +0545

    updated

--- controller/order.js
+++ controller/order.js
@@ -14,3 +14,38 @@ exports.postOrderItem = (req,res)=>{
     res.json({orderitem})
   })
 }
+
+
+exports.postOrder =async(req,res)=>{
+  const orderItems = req.body.orderItems
+
+  //to calculate total price
+  const totalPrices = await Promise.all(orderItems.map(async(orderItemId)=>{
+    const orderItems = await OrderItem.findById(orderItemId).populate('product','product_price')
+    const total = OrderItems.product.product_price*orderItem.quantity
+
+    return(total)
+  }))
+
+  const totalPrice = totalPrices.reduce((a,b)=>a+b,0)
+
+
+  let order = new Order({
+    orderItems:req.body.orderItems,
+    shippingAdress:req.body.shippingAddress,
+    city:req.body.city,
+    phone_number:req.body.phone,
+    zip:req.body.zip,
+    status:req.body.status,
+    user:req.body.user,
+    country:req.body.country,
+    totalPrice:totalPrices
+  })
+
+  await order.save((error,result)=>{
+    if(error || !result){
+      return res.status(400).json({error:"something went wrong"})
+    }
+    res.json({result})
+  })
+}
