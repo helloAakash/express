d22e3a406430131174a02f5074d0d145a9c8f17b
helloAakash <asissunuwar78@gmail.com>
2021-05-05 17:05:36 +0545

    updated

---
+++ React/ecommerce-app/src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch,Route} from 'reacat-router-dom'
+
+const Routes = () = >{
+  return{
+    <>
+    <Router>
+    <Switch>
+    <Route exact path="/" component={}/>
+
+    </Switch>
+
+    </Router>
+    </>
+  }
+}
+
+export default Routes
